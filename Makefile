bbad20f35d41ebf0cac3a4d3d623ef75503a5289
Abhinandan <akmmbhmishra.mishra@gmail.com>
2025-01-07 15:39:41 +0530

    new files

---
+++ Makefile
@@ -0,0 +1,14 @@
+GPPPARAMS= -m32 -fno-use-cxa-atexit -nostdlib -fno-builtin -fno-rtti -fno-exeptions -fno-leading-underscore
+ASPARAMS =--32
+objects = loader.o kernel.o
+%.o:%.cpp
+    g++ $(GPPPARAMS) -o $@ -c $<
+
+%.o:%.s
+    as $(ASPARAMS) -o $@ $<
+
+	mykernel.bin: linker.ld $(objects)
+	           ld $(LDPARAMS) -T $< -o $@ $(objects)
+
+install: mykernel.bin
+          sudo cp $< /boot/mykernel.bin
---
+++ kernel.cpp
@@ -0,0 +1,15 @@
+void printf(char* str){
+    unsigned short* VideoMemory = (unsigned short*)0xb8000;
+    for(int i=0;str[i]!='\0';i++){
+        VideoMemory[i]=(VideoMemory[i]& 0xFF00)|str[i];
+        //upper this is done we want only high bits to print a char by char
+        //and not changing bits which are responsibly for colorchange 
+        //thats why we are using short which merges two bits into one so that we
+        //not able to change the color bit as we not have to change that
+
+    }
+}
+extern "C" void kernelMain(void *multiboot_structure, unsigned int magicnumber){
+    printf("hello world");
+    while(1);
+}
